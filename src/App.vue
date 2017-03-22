454303bb1108586138d1b4ee308a77e01d84dbd4
Robert Kirsz <robert.kirsz@gmail.com>
2017-03-22 10:26:50 +0100

    Added component for testing WP connection

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <div v-html="trescWpisu" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      trescWpisu: ''
+    }
+  },
+  mounted () {
+    this.$http.get('http://dottipelotti.eu/wpbaza/wp-json/wp/v2/posts').then(response => {
+      this.trescWpisu = response.body[0].content.rendered
+    }, error => {
+      console.log(error)
+    })
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
@@ -7,6 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Home',
+      component: Home
     }
   ]
 })
