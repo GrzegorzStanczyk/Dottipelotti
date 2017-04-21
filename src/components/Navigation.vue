6de72d413382b3a9c360080c392ee12a5576cb88
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-04-21 16:17:57 +0200

    Flated style for wondows, and added div for separate title

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,38 +1,52 @@
 <template>  
     <nav role="navigation" :class="{ 'rotate': !rotateStyle }">
-      <router-link class="window home" to="/" exact >
-        <div class="door--left"></div>
-        <div class="door--right"></div>
+      <router-link class="home" to="/" exact >
+        <div class="window">
+          <div class="door--left"></div>
+          <div class="door--right"></div>
+        </div>        
         <span>{{navTitle ? 'dom' : 'home'}}</span>
       </router-link>
-      <router-link class="window mision" to="/misja">
-        <div class="door--left"></div>
-        <div class="door--right"></div>  
+      <router-link class="mision" to="/misja">
+        <div class="window">          
+          <div class="door--left"></div>
+          <div class="door--right"></div>  
+        </div>
         <span>{{navTitle ? 'misja' : 'mission'}}</span>
       </router-link>
-      <router-link class="window program" to="/program">
-        <div class="door--left"></div>
-        <div class="door--right"></div>  
+      <router-link class="program" to="/program">
+        <div class="window">
+          <div class="door--left"></div>
+          <div class="door--right"></div>  
+        </div>
         <span>program</span>
       </router-link>
-      <router-link class="window on-the-plate" to="/na-talerzu">
-        <div class="door--left"></div>
-        <div class="door--right"></div>  
+      <router-link class="on-the-plate" to="/na-talerzu">
+        <div class="window">
+          <div class="door--left"></div>
+          <div class="door--right"></div>  
+        </div> 
         <span>{{navTitle ? 'na talerzu' : 'on the plate'}}</span>
       </router-link>
-      <router-link class="window gallery" to="/galeria">
-        <div class="door--left"></div>
-        <div class="door--right"></div>  
+      <router-link class="gallery" to="/galeria">
+        <div class="window">
+          <div class="door--left"></div>
+          <div class="door--right"></div>  
+        </div>        
         <span>{{navTitle ? 'galeria' : 'gallery'}}</span>
       </router-link>
-      <router-link class="window staff" to="/kadra">
-        <div class="door--left"></div>
-        <div class="door--right"></div>  
+      <router-link class="staff" to="/kadra">
+        <div class="window">
+          <div class="door--left"></div>
+          <div class="door--right"></div>         
+        </div> 
         <span>{{navTitle ? 'kadra' : 'our staff'}}</span>
       </router-link>
-      <router-link class="window prices" to="/cennik">
-        <div class="door--left"></div>
-        <div class="door--right"></div>  
+      <router-link class="prices" to="/cennik">
+        <div class="window">        
+          <div class="door--left"></div>
+          <div class="door--right"></div>        
+        </div>  
         <span>{{navTitle ? 'cennik' : 'prices'}}</span>
       </router-link>
       <router-link class="contact" to="/kontakt"><span>{{navTitle ? 'kontakt' : 'contact'}}</span></router-link>
@@ -86,7 +100,6 @@ nav {
   // transition: background .2s ease-in; 
   transition: all .4s ease;
   &:hover {
-    color: #000000;
     box-shadow: 0px 0px 15px 1px rgba(0,0,0,0.2);
   }
   &:hover .door--right {        
@@ -97,17 +110,19 @@ nav {
     transform: rotateY(-130deg);
     box-shadow: 15px 5px 30px 5px rgba(0,0,0,0.3);
   }
-  & > span {
-    position: absolute;
-    width: 100%;
-    bottom: -30px;
-    text-align: center;
-  }
 }
 
-a {
+a {  
   color: inherit;
   text-decoration: none;
+  &:hover {
+    color: #000000;    
+  }
+  & span {
+    display: block;
+    margin-top: 5px;
+    text-align: center;
+  }
 }
 
 .rotate .door--left {
@@ -125,8 +140,8 @@ a {
     left: -1px;
     width: 50%;
     height: 100%;
-    box-shadow: 1px 1px 5px 1px rgba(0,0,0,0.3);
-    background: linear-gradient(to right, rgba(226,226,226,1) 0%, rgba(252,255,240,1) 36%, rgba(252,255,240,1) 100%);    
+    box-shadow: 0px 0px 1px 0px rgba(0,0,0,0.3);
+    background: linear-gradient(to right, rgba(226,226,226,1) 0%, rgba(252,255,240,1) 15%, rgba(252,255,240,1) 100%);    
     transform-origin: 0 50%;
     transition: all .5s ease;
     &::before {
@@ -139,31 +154,31 @@ a {
     }
 }
 
-.window:nth-child(1) .door--left::before {
+a:nth-child(1) .door--left::before {
     top: 35%;
     background-color: rgb(254,116,175);
 }
-.window:nth-child(2) .door--left::before {
+a:nth-child(2) .door--left::before {
     top: 50%;
     background-color: rgb(0,209,203);
 }
-.window:nth-child(3) .door--left::before {
+a:nth-child(3) .door--left::before {
     top: 45%;
     background-color: rgb(255,225,0);
 }
-.window:nth-child(4) .door--left::before {
+a:nth-child(4) .door--left::before {
     top: 30%;
     background-color: rgb(251,182,58);
 }
-.window:nth-child(5) .door--left::before {
+a:nth-child(5) .door--left::before {
     top: 55%;
     background-color: rgb(194,225,123);
 }
-.window:nth-child(6) .door--left::before {
+a:nth-child(6) .door--left::before {
     top: 35%;
     background-color: rgb(255,70,153);
 }
-.window:nth-child(7) .door--left::before {
+a:nth-child(7) .door--left::before {
     top: 40%;
     background-color: rgb(247,225,65);
 }
@@ -174,14 +189,18 @@ a {
     right: 0;
     width: 50%;
     height: 100%;
-    box-shadow: 0px 0px 5px 1px rgba(0,0,0,0.3);
-    background: linear-gradient(to left, rgba(226,226,226,1) 0%, rgba(252,255,240,1) 36%, rgba(252,255,240,1) 100%);
+    box-shadow: 0px 0px 1px 0px rgba(0,0,0,0.3);
+    background: linear-gradient(to left, rgba(226,226,226,1) 0%, rgba(252,255,240,1) 15%, rgba(252,255,240,1) 100%);
     transform-origin: 100% 0%;
     transition: all .4s ease;
 }
 
-.router-link-active{
+.router-link-active {
   color: #000000;
+}
+
+.router-link-active .window{
+  box-shadow: 0px 0px 15px 1px rgba(0,0,0,0.2);  
 }
 
 .router-link-active .door--right {
@@ -194,31 +213,31 @@ a {
     box-shadow: 15px 5px 30px 5px rgba(0,0,0,0.3);
 }
 
-.home {
+.home .window {
   background: url(../../static/images/home-akt.png) no-repeat center;
 }
 
-.mision {
+.mision .window {
   background: url(../../static/images/misja-akt.png) no-repeat 50% 54%;
 }
 
-.program {
+.program .window {
   background: url(../../static/images/program-akt.png) no-repeat center;
 }
 
-.on-the-plate {
+.on-the-plate .window {
   background: url(../../static/images/na-talerzu-akt.png) no-repeat 50% 57%;
 }
 
-.gallery {
+.gallery .window {
   background: url(../../static/images/galeria-akt.png) no-repeat 50% 56%;
 }
 
-.staff {
+.staff .window {
   background: url(../../static/images/kadra-akt.png) no-repeat 50% 56%;  
 }
 
-.prices {
+.prices .window {
   background: url(../../static/images/cennik-akt.png) no-repeat 50% 56%;
 }
 
@@ -227,7 +246,6 @@ a {
   background: url(../../static/images/kontakt-akt.png) no-repeat center 0;
   color: #ffffff;
 }
-
 
 .contact {
   position: absolute;
